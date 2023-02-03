b884e66861fd3ea0aea5a2b39d01108ca06130c1
Shamak Patel <shamak@dhcp-172-29-17-10.guest.uc.edu>
2023-02-03 14:01:40 -0500

    Initial commit

---
+++ src/salesTax.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+    double purchasePrice = 10;
+    double salesTax = 0.05;
+    double total = purchasePrice * salesTax;
+        System.out.println(" Purchase price is" + purchasePrice);
+        System.out.println("Purchase price with the Sales tax" + total);
+    }
+}
