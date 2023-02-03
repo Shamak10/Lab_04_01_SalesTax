6371047861fdcd5d01edd57190d4bbb59f23c05f
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-03 14:16:37 -0500

    Update salesTax.java

--- src/salesTax.java
+++ src/salesTax.java
@@ -1,9 +1,9 @@
-public class main {
+public class salesTax {
     public static void main(String[] args) {
-    double purchasePrice = 10;
-    double salesTax = 0.05;
-    double total = purchasePrice * salesTax;
-        System.out.println("Purchase price is" + purchasePrice);
-        System.out.println("salesTax is" + total);
+        double purchasePrice = 10;
+        double salesTax = 0.05;
+        double total = purchasePrice * salesTax ;
+        System.out.println("Purchase price is " + purchasePrice);
+        System.out.println("salesTax is " + total);
     }
 }
